9bb494aada996addf933e5b69e511ab051cddecb
jbzm <125964590@qq.com>
2018-06-04 19:55:52 +0800

    lol

---
+++ eureka-server/src/main/java/top/jbzm/social/eurekaserver/EurekaServerApplication.java
@@ -0,0 +1,18 @@
+package top.jbzm.social.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author jbzm
+ * @date 2018下午6:44
+ **/
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+}
